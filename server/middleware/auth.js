d1693282dcdea704f741735a40721ec1b740f7f9
zhagm <zhagdev@gmail.com>
2020-04-21 12:53:44 -0700

    Fix server error

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,16 +6,18 @@ function auth(req, res, next) {
   // get token from client
   const token = req.header("x-auth-token");
 
-  if (!token) res.status(401).send("Token missing, user not authorized");
-
-  try {
-    // Verify token
-    let decoded = jwt.verify(token, jwtSecret);
-    // Store user id in req object;
-    req.user = decoded;
-    next();
-  } catch (err) {
-    res.status(400).send("Token is not valid");
+  if (!token) {
+    res.status(401).send("Token missing, user not authorized");
+  } else {
+    try {
+      // Verify token
+      let decoded = jwt.verify(token, jwtSecret);
+      // Store user id in req object;
+      req.user = decoded;
+      next();
+    } catch (err) {
+      res.status(400).send("Token is not valid");
+    }
   }
 }
 
