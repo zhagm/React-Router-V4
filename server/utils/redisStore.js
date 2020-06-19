84f0c89da7ffd17be70964414ffd9dc6f1bc9bee
zhagm <zhagdev@gmail.com>
2020-06-19 09:52:05 -0700

    Add redis heroku setup code

--- server/utils/redisStore.js
+++ server/utils/redisStore.js
@@ -1,5 +1,10 @@
 const redis = require("redis");
-const client = redis.createClient();
+let client;
+if (process.env.REDIS_URL) {
+  client = redis.createClient(process.env.REDIS_URL, { no_ready_check: true });
+} else {
+  client = redis.createClient();
+}
 
 client.FLUSHALL();
 
